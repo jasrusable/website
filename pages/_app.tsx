f35d53c2a14b50c73302799fdd5e0622df047001
Jason Russell <jasrusable@gmail.com>
2021-12-07 09:06:05 +0200

    Add metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Jason Russell</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>{" "}
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
